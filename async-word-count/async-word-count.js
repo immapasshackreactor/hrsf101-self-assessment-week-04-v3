2ad0e7001e4f97b078f7c3110caa66cf5543962d
David Hong <davydhong@gmail.com>
2018-08-27 09:59:30 -0700

    finshed two, attemped server

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -2,19 +2,45 @@ var fs = require('fs');
 var path = require('path');
 
 var getWordCount = function(filePath, callback) {
-  fs.readFile(filePath, 'utf-8', function(err, data) {
-    if (err) {
-      callback(err, null);
-      return;
-    }
+	fs.readFile(filePath, 'utf-8', function(err, data) {
+		if (err) {
+			callback(err, null);
+			return;
+		}
 
-    var wordCount = data.trim().split(' ').length;
-    callback(null, wordCount);
-  });
+		var wordCount = data.trim().split(' ').length;
+		callback(null, wordCount);
+	});
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+	var counter = 0;
+	// Invoke getWordCount twice in nested callback form;
+	// First getWordCount
+	getWordCount(filePathOne, (err, data) => {
+		if (err) {
+			callback(err, null);
+			return;
+		}
+		counter += data;
+		// Second getWordCount:
+		getWordCount(filePathTwo, (err, data2) => {
+			if (err) {
+				callback(err, null);
+				return;
+			}
+			counter += data2;
+			callback(null, counter);
+		});
+	});
 };
+
+getTotalWordCount(
+	'/Users/davidhong/Desktop/HackReactor/hrsf101-self-assessment-week-04-v3/async-word-count/files/fileTwo.txt',
+	'/Users/davidhong/Desktop/HackReactor/hrsf101-self-assessment-week-04-v3/async-word-count/files/fileOne.txt',
+	(err, data) => console.log(data)
+);
+
+//79 + 58 = 137;
 
 module.exports = getTotalWordCount;
