f4ec50500115ee922cdcd3cad02ec6aa3d560ded
Tom Costello <tcostellosf25@gmail.com>
2018-08-27 09:55:34 -0700

    Updated with Attempted Code

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,12 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// Option 6 ... we want to show a many to many relationship  
 
 var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// Option 5 ... we want to show that a publisher has one genre (so include genre_id) and that one genre could have many publishers (so include publisher_id)
 
 var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
 // TODO: Write a comment here explaining your choice
+// Option 6 ... we want to create a many to many relationship between authors and genres...  therefore we use a join table
