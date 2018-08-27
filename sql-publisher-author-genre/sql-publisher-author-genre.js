bfa8b5b174f45625ff37e47b337bf5031a13a345
Jerry Wu <wuj828@gmail.com>
2018-08-27 09:51:47 -0700

    I believe I completed all prompts accordingly. Last prompt was somewhat confusing.

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -1,5 +1,5 @@
 var Schemas = {
-  FIX_ME: 'wrong answer',
+  FIX_ME: 'option 4, option 6, option 6',
   OPTION_0: 'option 0',
   OPTION_1: 'option 1',
   OPTION_2: 'option 2',
@@ -10,10 +10,17 @@ var Schemas = {
 };
 
 var sqlPublisherAuthorSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+/* I selected option 4 because the prompt indicates this is a one-to-many relationship. Hence, author does not need to
+   monitor the IDs of its publishers. As there is only one genre, it is irrelevant to the schema.
+*/
 
 var sqlPublisherGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+/* I selected option 6 because the prompt indicates a many-to-many relationship. Thus, I felt that the intermediary table,
+   the name of which I forget, would allow us to quickly look-up which authors belonged to which genres (exactly like the
+   example Fred gave during lecture regarding classes and students).
+*/
 
 var sqlAuthorGenreSchema =  Schemas.FIX_ME; // TODO: Update this constant
-// TODO: Write a comment here explaining your choice
+/* I selected option 6 for the same reason I selected option 6 for publisher/genre--because of the many-to-many relationship
+   this particular table intends on storing.
+*/
