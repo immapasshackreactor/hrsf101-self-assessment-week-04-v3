17955289e16613afb43e45d932a11f19364de51c
Mitchell Vandehaar <mitchvandehaar@gmail.com>
2018-08-27 09:43:53 -0700

    finished async-word-count

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -1,20 +1,43 @@
 var fs = require('fs');
 var path = require('path');
 
+// var filePath = (__dirname + 'fileOne.txt')
+
 var getWordCount = function(filePath, callback) {
   fs.readFile(filePath, 'utf-8', function(err, data) {
     if (err) {
+      console.log(err)
       callback(err, null);
       return;
     }
-
     var wordCount = data.trim().split(' ').length;
+    console.log("wordCount = ", wordCount)
     callback(null, wordCount);
   });
 };
 
-var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
-};
 
-module.exports = getTotalWordCount;
+
+var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
+  var total = 0;
+
+  getWordCount(filePathOne, function(err, countOne) {
+    console.log('filePathOne WC: ', countOne);
+    total += countOne;
+
+    getWordCount(filePathTwo, function(err , countTwo) {
+      console.log('filePathTwo WC: ', countTwo);
+      total += countTwo;
+      // callback(total)
+      return total;
+    });
+  });
+  
+
+}
+
+
+
+module.exports = getTotalWordCount('../async-word-count/files/fileOne.txt', '../async-word-count/files/fileTwo.txt')
+// console.log(getTotalWordCount('../async-word-count/files/fileOne.txt'), total())
+console.log(getTotalWordCount)
