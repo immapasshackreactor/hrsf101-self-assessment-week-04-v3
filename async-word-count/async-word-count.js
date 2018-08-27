78ff2f90370b1c89fc685d64a47597b727cc7669
Mjones13 <jonesmal@umich.edu>
2018-08-27 09:58:36 -0700

    submit

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -14,7 +14,54 @@ var getWordCount = function(filePath, callback) {
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+	var totalWordCount = 0;
+
+	getWordCount(filePathOne, function(err, wordCount) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		totalWordCount += wordCount;
+	});
+
+	getWordCount(filePathTwo, function(err, wordCount) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		totalWordCount += wordCount;
+	});
+
+
+  // fs.readDir(filePathOne, 'utf-8', function(err, files) {
+  // 	if (err) {
+  // 		callback(err, null);
+  // 		return;
+  // 	}
+  // 	files.forEach(function(file) {
+  // 		// console.log('here are all the files:', file);
+  // 		getWordCount(file, function(err, wordCount) {
+  // 			if (err) {
+  // 				console.error(err);
+  // 				return;
+  // 			}
+  // 			totalWordCount += wordCount;
+  // 		});
+  // 	});
+
+  // 	fs.readDir(filePathTwo, 'utf-8', function(err, files) {
+  // 		files.forEach(function(file) {
+  // 			// console.log('here are the second files:' file);
+  // 			getWordCount(file, function(err, wordCount) {
+  // 			if (err) {
+  // 				console.error(err);
+  // 				return;
+  // 			}
+  // 			totalWordCount += wordCount;
+	 //  		})
+  // 		});
+  // 	});
+  return totalWordCount;
 };
 
 module.exports = getTotalWordCount;
--- node-req-count/server.js
+++ node-req-count/server.js
@@ -8,10 +8,41 @@ var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
 
+  //I need to make a post request to the url with the specific endpoint
+    //go to the property specified on the endpoint of globalCounter
+    //increment it
+    //then return it as part of the response
+
+
+  //Really ran out of time on this one because my computer shut down on the way here and
+  //I had to boot it back up, old 2012 macbook so couldnt code until 9:20...
   if (request.method === 'POST') {
-    // YOUR CODE HERE
+    var property = endpoint.slice(1);
+    request.post({
+        url: `http://localhost/${endpoint}`,
+        function(error, response, body){
+            body[endpoint] += 1;
+    })
+
+    request.get(endpoint, function(req, res) {
+
+
+    response.writeHead(201, { 'Content-Type': 'application/json'});
+    response.end();
+    });
   } else if (request.method === 'GET') {
-    // YOUR CODE HERE
+    request.get(endpoint, function(req, res) {
+      var property = endpoint.slice(1);
+      var val = res.globalCounter.property;
+
+      if (val === undefined) {
+        response.end();
+      } else {
+        response.end(JSON.stringify(val));
+      }
+    });
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end();
   } else {
     response.statusCode = 404;
     response.end();
