fd91e1d599e482b70d4b84d27245e5fb83cf95b2
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-27 09:58:08 -0700

    attempts SA prompts

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -14,7 +14,20 @@ var getWordCount = function(filePath, callback) {
 };
 
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+  //I: two files and a callback that accepts the sum of words in each file
+  //O: callback that takes in the combined sum of words in each file
+  //strategy: call getWordCount for each file and pass the 
+    //call getWordCount
+    getWordCount(filePathOne, filePathTwo, (err, data) => {
+      if (err) {
+        throw err;
+        console.log('there was an error');
+      }
+      callback(null, filePathOne, filePathTwo);
+    })
+      //if error, callback err, err, null 
+      //else pass files to wordcount with err set to null 
+
 };
 
 module.exports = getTotalWordCount;
