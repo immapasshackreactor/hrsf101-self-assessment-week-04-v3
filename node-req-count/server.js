a6b958e64b04adf92c920ca554b29ad187f9c9ca
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-08-27 09:29:57 -0700

    Add GET and POST functionality for node-req-count

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -7,11 +7,35 @@ var globalCounter = {};
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
+  const prop = endpoint.substr(1);
 
   if (request.method === 'POST') {
-    // YOUR CODE HERE
+    // let data = [];
+    // request.on('data', (chunk) => {
+    //   data.push(chunk);
+    // });
+    // request.on('end', () => {
+    //   const message = Buffer.concat(data).toString();
+    //   let route =
+    //   if (message === 'dogs')
+    // });
+    if (globalCounter[prop] === undefined) {
+      globalCounter[prop] = 1;
+    } else {
+      globalCounter[prop] += 1;
+    }
+    response.statusCode = 201;
+    response.end();
+    console.log(globalCounter[prop]);
   } else if (request.method === 'GET') {
-    // YOUR CODE HERE
+    let result = '';
+    if (globalCounter[prop] !== undefined) {
+      result = globalCounter[prop];
+    }
+    console.log('result', result);
+    response.statusCode = 200;
+    response.write(result.toString());
+    response.end();
   } else {
     response.statusCode = 404;
     response.end();
