9938e1369362f845d925d0dcde13713fa96a125d
KEOLA MORSE <keolamorse@KEOLAs-MacBook-Pro.local>
2018-08-28 02:26:13 -0700

    refactored

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -1,27 +1,32 @@
 var url = require('url');
 var http = require('http');
 var path = require('path');
-
 var globalCounter = {};
 
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
 
+  var incrementer = function(property) {
+    if (globalCounter[property] === undefined) {
+      globalCounter[property] = 1;
+    } else {
+      globalCounter[property] += 1;
+    }
+  };
+
   if (request.method === 'POST') {
-     response.property = '';
-     response.endpoint = '';
-     globalCounter++;
-
+    incrementer(property);
   } else if (request.method === 'GET') {
-    response.endpoint = '';
-
-
+      if (globalCounter[property]) {
+        return globalCounter[property].toString();
+    } else {
+      response.end();
+    }
   } else {
     response.statusCode = 404;
     response.end();
-  }
+  }  
 });
-
 // Do not edit this line
 module.exports = server;
