bce4c249ef5713f142dafd7e5412270f545839a4
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-02 16:12:34 -0700

    implements server correctly

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -8,26 +8,23 @@ var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
 
-  if (request.method === 'POST' && request.url === property) {
-    //create place to store data
-    let body = [];
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = JSON.stringify(body);
-      response.end(body);
-    });
-    // YOUR CODE HERE
-  } else if (request.method === 'GET' && request.url === property) {
-    response.on('data' () => {
-     let body = JSON.stringify(data);
-     response.end(data);
-    });
-    
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
+  if (request.method === 'POST') {
+    if(!globalCounter[property]){
+      globalCounter[property] = 1;
+    } else {
+      globalCounter[property] ++;
+    }
+    response.sendStatus(201);
+    } else if (request.method === 'GET') {
+      if (globalCounter[property]) {
+        response.status(200).send(JSON.stringify(globalCounter[property]))
+      } else {
+        response.sendStatus(200).send(JSON.stringify());
+      }
+    } else {
+      response.sendStatus(404);
+    }
+
 });
 
 // Do not edit this line
