21f32cb1fe5486bb8c3d342c02126ca900e7cce4
Huan L <huanl@huans-mbp.lan>
2018-09-03 13:21:45 -0700

    Reviewed and completed the SA (part 1 and part 2), for week 5

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -4,23 +4,35 @@ var path = require('path');
 
 var globalCounter = {};
 
+// Note: not sure what the use of 'endpoint' and 'property' (given codes) are
+// Chris L used properpty; but it is only optional for here/now
 var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
+  // console.log('endpoint ' + endpoint);  // Endpoint's count on globalCounter
   var property = endpoint.replace(/^\//, '');
+  // console.log('property ' + property);
 
-  if (request.method === 'POST') {
+  if (request.method === 'POST' && request.url === '/dogs') {
     // YOUR CODE HERE
-    let body = [];
-    request.on('error', err) => {
-      console.log(err);
-    }).on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {});
-
-  } else if (request.method === 'GET') {
+    if(!globalCounter.dog) {
+      globalCounter.dog = 1;
+    } else {
+      globalCounter.dog = globalCounter.dog + 1;
+    }
+    console.log('After POST, globalCounter.dog = ' + globalCounter.dog);
+    var body = JSON.stringify(globalCounter.dog);
+    response.end(body);
+  } else if (request.method === 'GET' && request.url === '/dogs') {
     // YOUR CODE HERE
-    let response.body = globalCounter;
-
+    console.log('Per GET, globalCounter.dog = ' + globalCounter.dog);
+    if (!globalCounter.dog) {
+      response.end('No dog posted yet');
+    } else {
+      var body = JSON.stringify(globalCounter.dog);
+      response.end(body);
+    }
+  } else if (request.method === 'GET' && request.url !== '/dogs') {
+    reponse.end();  // Empty response, for undefined route (the previous line)
   } else {
     response.statusCode = 404;
     response.end();
